2a00b06ffaa35f314d3aad7315a9b73d1b0ee9bb
Aneeb <aneebpaloth74@gmail.com>
2023-08-20 03:50:59 -0700

    login and signup page styled

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.Home,name='home'),
+    path('login',views.Login,name='login'),
+    path('signup',views.Signup,name='signup')
 ]
--- base/views.py
+++ base/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
+def Home(request):
     return render(request,'base/home.html')
+
+def Login(request):
+    page = 'login'
+    context = {"page":page}
+    return render(request,'base/login.html',context)
+
+def Signup(request):
+    return render(request,'base/login.html')
