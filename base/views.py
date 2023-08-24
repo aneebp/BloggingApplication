bdfffd2c029527478122c5d4c672c9bb7d0c3555
Aneeb <aneebpaloth74@gmail.com>
2023-08-24 10:17:40 -0700

    user authentication login added

--- base/views.py
+++ base/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate ,login,logout
+from .models import User,Blogge,Comment
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,6 +12,22 @@ def Home(request):
 
 def Login(request):
     page = 'login'
+    # if request.user.is_authenticated:
+    #     return redirect('home')
+    if request.method == 'POST':
+        username = request.POST.get("username").lower()
+        password = request.POST.get("password")
+        # try:
+        #     user = User.objects.get(username=username)
+        # except:
+        #     messages.error(request,'User does not exit ')
+        user = authenticate(request , username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'username or password does not exist')   
+             
     context = {"page":page}
     return render(request,'base/login.html',context)
 
